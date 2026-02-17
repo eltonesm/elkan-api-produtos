b305d478525ae4c4d35fdb6459acce1af7ad57de
Elton Marques <eltonesm@gmail.com>
2026-02-16 22:58:56 -0300

    refactor: implement hexagonal architecture structure and add global exception handler

---
+++ src/main/java/com/elkanplace/ms_produtos/infrastructure/adapter/input/exception/APIException.java
@@ -0,0 +1,65 @@
+package com.elkanplace.ms_produtos.infrastructure.adapter.input.exception;
+
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+@Getter
+@Log4j2
+public class APIException extends RuntimeException {
+	private HttpStatus statusException;
+	private ErrorApiResponse bodyException;
+
+	private APIException(HttpStatus statusException, String message, Exception e) {
+		super(message, e);
+		this.statusException = statusException;
+		this.bodyException = ErrorApiResponse.builder()
+				.message(message)
+				.description(getDescription(e))
+				.build();
+	}
+
+	private APIException(String message, Exception e) {
+		super(message, e);
+		this.bodyException = ErrorApiResponse.builder()
+				.message(message)
+				.description(getDescription(e))
+				.build();
+	}
+
+	public static APIException build(HttpStatus statusException, String message) {
+		return new APIException(statusException, message, null);
+	}
+
+	public static APIException build(HttpStatus statusException, String message, Exception e) {
+		log.error("Exception: ", e);
+		return new APIException(statusException, message, e);
+	}
+
+	public static APIException build(String message, Exception e) {
+		log.error("{}: {}", message, e.getMessage());
+		return new APIException(message, e);
+	}
+
+	private String getDescription(Exception e) {
+		return Optional.ofNullable(e)
+				.map(APIException::getMessageCause).orElse(null);
+	}
+
+	private static String getMessageCause(Exception e) {
+		return e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
+	}
+
+
+
+	public ResponseEntity<ErrorApiResponse> buildErrorResponseEntity() {
+		return ResponseEntity
+				.status(statusException)
+				.body(bodyException);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
