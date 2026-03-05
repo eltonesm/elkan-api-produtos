6acce5e7aa53c6641306fc0dd3ee54018947728c
Elton Marques <eltonesm@gmail.com>
2026-03-05 18:06:41 -0300

    feat(products): add createdAt and updatedAt fields and create indexes for name and price

--- src/main/java/com/elkanplace/api_produtos/domain/model/Product.java
+++ src/main/java/com/elkanplace/api_produtos/domain/model/Product.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
@@ -16,18 +17,25 @@ public class Product {
     private final BigDecimal price;
     private final Integer stock;
 
+    private final Instant createdAt;
+    private final Instant updatedAt;
+
     public static Product create(
             String name,
             String description,
             BigDecimal price,
             Integer stock
     ) {
+        Instant now = Instant.now();
+
         return Product.builder()
                 .id(UUID.randomUUID().toString())
                 .name(name)
                 .description(description)
                 .price(price)
                 .stock(stock)
+                .createdAt(now)
+                .updatedAt(now)
                 .build();
     }
 
@@ -42,6 +50,8 @@ public class Product {
                 .description(this.description)
                 .price(this.price)
                 .stock(this.stock - quantity)
+                .createdAt(this.createdAt)
+                .updatedAt(Instant.now())
                 .build();
     }
 }
--- src/main/java/com/elkanplace/api_produtos/infrastructure/adapter/output/document/ProductDocument.java
+++ src/main/java/com/elkanplace/api_produtos/infrastructure/adapter/output/document/ProductDocument.java
@@ -2,9 +2,11 @@ package com.elkanplace.api_produtos.infrastructure.adapter.output.document;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Document(collection = "products")
 @Getter
@@ -16,8 +18,13 @@ public class ProductDocument {
     @Id
     private String id;
 
+    @Indexed
     private String name;
     private String description;
+    @Indexed
     private BigDecimal price;
     private Integer stock;
+
+    private Instant createdAt;
+    private Instant updatedAt;
 }
